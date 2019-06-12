a61aa32d03ce93bbe4f7aca27c6da88f958964df
generic-tag-3052 <christopherchoi97@gmail.com>
2019-06-12 01:19:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var greeting = "Hello, World!";
 
 var specialGuest = "Neil deGrasse Tyson";
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = specialGuest + "!";
 
-var topic = "space";
+var topic = " ";
 
 var conversation = `${topic}`;
